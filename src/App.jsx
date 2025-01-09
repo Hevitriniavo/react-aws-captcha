17660858489bca9bae353b08d01149d4b9e24b4b
Hevitriniavo <hei.tantely@gmail.com>
2025-01-09 05:27:07 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  console.log('AWS WAF environment loaded:', window.AWS_WAF_ENV);
+
+  return (
+    <>
+
+      <button>show captcha</button>
+    </>
+  )
+}
+
+export default App
